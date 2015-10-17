86a51ec65d92e761153ec391afadf3ef86e062df
Nino Miletich <nino@miletich.me>
2015-10-17 22:56:18 +0200

    add gem bin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -36,5 +36,8 @@ source ~/.config/fish/secrets/*.fish
 # Add $HOME/bin to $PATH
 set -gx PATH $HOME/bin $PATH
 
+# Add
+set -gx PATH $HOME/.gem/ruby/2.2.0/bin $PATH
+
 # Use XTERM
 set -gx TERM xterm-256color
