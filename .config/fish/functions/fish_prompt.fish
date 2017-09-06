4b6edc1ef0be8c6fb0e45091d7549a8d3ae5ef57
Nino Miletich <ninomiletich@gambitresearch.com>
2017-09-06 11:47:48 +0100

    add current git branch name to the prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,6 +1,8 @@
 function fish_prompt --description 'Write out the prompt'
   set -l color_cwd
   set -l suffix
+  set -l git_branch (git branch ^/dev/null | cut -d' ' -f2)
+  set -l branch_name ''
 
   switch $USER
   case root toor
@@ -15,5 +17,9 @@ function fish_prompt --description 'Write out the prompt'
     set suffix '>'
   end
 
-  echo -n -s (set_color blue) "$USER" (set_color normal) ' ' (set_color $color_cwd) (prompt_pwd) (set_color normal) "$suffix "
+  if [ "$git_branch" ]
+    set branch_name ' ' (set_color yellow) $git_branch (set_color normal)
+  end
+
+  echo -n -s (set_color blue) "$USER" (set_color normal) ' ' (set_color $color_cwd) (prompt_pwd) (set_color normal) $branch_name "$suffix "
 end
