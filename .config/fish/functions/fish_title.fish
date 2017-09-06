80f5725ce2c10d19003204850926f12d297df54b
Nino Miletich <ninomiletich@gambitresearch.com>
2017-09-06 11:48:25 +0100

    remove legacy stuff

--- .config/fish/functions/fish_title.fish
+++ .config/fish/functions/fish_title.fish
@@ -1,32 +1,8 @@
-function ssh_title -d "Set SSH title"
-  set -l hostname
-
-  echo (echo $argv[1]| sed 's| -[[:alnum:]]*||g' | cut -d' ' -f 2) | read -la hostname
-
-  # Misc cases
-  set hostname (echo $hostname | sed 's|ssh2.diversentity.net$|diversentity|g')
-
-  # Plus Hosting cases
-  set hostname (echo $hostname | sed 's|.plusvps.com$||g')
-  set hostname (echo $hostname | sed 's|.mojsite.com$||g')
-  set hostname (echo $hostname | sed 's|.plus.hr$||g')
-
-  echo $hostname
-  return 0
-end
-
 function fish_title
   if set -q context_title
     echo $context_title
-  else if set -q argv[1]
-
-    switch (echo $argv[1] | cut -d' ' -f 1);
-    case ssh;
-      ssh_title $argv[1]
-    end
-
   else
-    hostname
+    echo $USER (prompt_pwd)
   end
 
   return 0
