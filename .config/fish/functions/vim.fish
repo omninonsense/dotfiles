4b2fa396bf41141464a547b1643c062eefe9e5a8
Nino Miletich <nino@miletich.me>
2018-01-05 01:09:24 +0000

    I gotta stop listening to sad love songs when doing this config shit. I might start associating configuration with heartbreak and that ain't good

--- .config/fish/functions/vim.fish
+++ .config/fish/functions/vim.fish
@@ -1,4 +1,3 @@
-function vim --description 'An alias that starts tarts emacs instead of vim'
-
-	emacsclient -nc -a "" $argv
+function vim --description 'An alias that starts emacs instead of vim'
+  emacsclient -n -a "" $argv
 end
